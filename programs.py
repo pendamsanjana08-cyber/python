18c740aa1c18b8576156cd3c2ebeb312a2b3c0c6
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-02-16 10:23:51 -0800

    added variables

---
+++ programs.py
@@ -0,0 +1,43 @@
+name="sanjana"#string #1
+age=19#integer
+height=5.6#float
+print(name)
+print(age)
+print(height)
+
+name=(input("enter a name:"))#2
+age=eval(input("enter age:"))
+print(f"my name is {name} and iam {age} years old")
+
+x,y,z=10,20,30
+print(x+y+z)
+
+
+'''Question 1
+
+Create three variables:
+
+* `name` (string)
+* `age` (integer)
+* `height` (float)
+
+Print their values and their data types.
+
+Question 2
+
+Take input from the user for:
+
+* Name
+* Age
+
+Convert age into integer and print:
+`My name is ___ and I am ___ years old.`
+
+ 🧠 Question 3
+
+Assign multiple variables in one line:
+
+* `x = 10`
+* `y = 20`
+* `z = 30`
+print sum of 3 numbers'''
