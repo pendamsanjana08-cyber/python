38b000a24f25f99dc47052e5ce6251cf2c06fafb
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-02-17 10:29:05 -0800

    python variables practice

---
+++ varibles 2.py
@@ -0,0 +1,38 @@
+'''🧠 Question 1
+
+Take a number as input from user.
+Print:
+
+The original value
+
+Its type
+
+Convert it to float and print type again
+
+🧠 Question 2
+
+Assign:
+
+x, y, z = 5, 5.5, "5"
+
+
+Print:
+
+Value of each
+
+Type of each '''
+#1
+number=eval(input("enter a number:"))
+print(number)
+print(type(number))
+number1=float(number)
+print(number1)
+print(type(number1))
+#2
+x,y,z=5,5.5,"5"
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
