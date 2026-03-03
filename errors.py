54589161f3706e7ed224b3b442a2a59ec34fb6c0
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-03-03 08:39:22 -0800

    sloved error

---
+++ errors.py
@@ -0,0 +1,20 @@
+print("Hello World) #syntax error no proper syntax
+      
+a = 10
+b = 0
+print(a / b)#zero division error cant divide by 0
+
+num = 5
+if num = 5:# to check whether the ststement i true we have to take ==
+    print("Equal")#invalid syntax 
+
+x = 10
+y = 5
+z = x + y#logical error
+print("Multiplication is", z)# no error but the operator doesnt match the print statement
+
+def add(a, b):
+    return a + b
+
+result = add(5)
+print(result)#type error because only 1 value assigned
