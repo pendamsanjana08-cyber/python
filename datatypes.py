0078c410b47e48ed2b7cdd9e43916a521d0b4256
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-03-04 10:26:53 -0800

    datatypes done

---
+++ datatypes.py
@@ -0,0 +1,31 @@
+#Create a variable storing your age and print its datatype.
+age=18
+print(type(age))
+
+#Create a list containing 3 colors and print the list and its datatype.
+x=["blue","black","white"]
+print(x)
+print(type(x))
+
+#Create a dictionary with keys name and age. Print the dictionary and its datatype.
+s={"name":"sanj","age":18}
+print(s)
+print(type(s))
+
+#Create a list with 4 different numbers and print its datatype.
+l=[1,2,3,4]
+print(type(l))
+
+"""Create a variable data that contains:
+your name (string)
+your age (integer)
+Store all three values inside one single list and print:
+The list
+The datatype of the list"""
+
+name="sanj"
+age=19
+list=[name,age]
+print(list)
+print(type(list))
+
