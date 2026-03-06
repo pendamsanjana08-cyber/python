114affbe3d925350a40daf4ffd9ce2922b6b0f04
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-03-06 09:43:01 -0800

    basic strngs

---
+++ python/functions,variables/basic strngs.py
@@ -0,0 +1,48 @@
+#1 Input a string and print it.
+s="sanj"
+print(s)
+
+#2 take a string nd Print String Length
+name="dimpu"
+print(name)
+print(len(name))
+
+#3 Input a string and print the first character.
+e="sanjana"
+print(e)
+print(e[0])
+
+#4 Print Last Character
+#Input a string and print the last character.
+#Input: apple
+#Output: e
+fruit="apple"
+print(fruit)
+print(fruit[-1])
+
+#5 Concatenate Two Strings
+a="hello"
+b=" this is sanjana"
+print(a+b)
+
+#6 Print Characters Using Loop
+#Input: dog
+#Output:
+#d
+#o
+#g
+u="dog"
+for x in u:
+ print(x)
+
+#7 Repeat String
+m="sanjana "
+n=3
+print(m*n)
+
+#8 Convert to Uppercase
+d="lion"
+print("before conversion:",d)
+print("after conversion:",d.upper())
+
+
