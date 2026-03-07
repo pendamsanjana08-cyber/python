043406cc1f0c54f2b39e45e40459d60db725e14c
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-03-07 10:22:53 -0800

    type casting

---
+++ python/functions,variables/type casting.py
@@ -0,0 +1,40 @@
+"""string to Integer
+Write a program:
+a = "50"
+Convert it into an integer
+Add 25 to it
+Print the result."""
+a="50"
+b=int(a)+25
+print(b)
+
+"""Float to Integer
+Write a program:
+num = 18.75
+Convert it into an integer
+Print the value and its data type."""
+num=18.75
+c=int(num)
+print(c)
+print(type(c))
+
+"""integer to String
+Write a program:
+x = 200
+Convert it into a string
+Print "The number is " with the value."""
+x=200
+y=str(x)
+print("The number is:",y)
+
+"""Mixed Type Addition
+Write a program:
+a = "10"
+b = "20"
+Convert both values into integers
+Find their sum and print it."""
+a="10"
+b="20"
+c=int(a)
+d=int(b)
+print(c+d)
