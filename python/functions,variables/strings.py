f139a6ed19859af5d4de2d47eae24c4052e5bbb7
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-03-09 09:12:38 -0700

    string oprations

---
+++ python/functions,variables/strings.py
@@ -0,0 +1,50 @@
+#used to check whether a string is in varaiable are not we use in
+s="she is briliant"
+print("is" in s)
+
+#To check whether it is not in present the varaiable
+s="she is brilliant"
+print("good" not in s)
+
+#Create a string "python" and print the first 3 characters using slicing.
+p="python"
+print(p[0:3])
+
+#Create a string "programming" and print the last 4 characters using slicing.
+m="programming" 
+print(m[-4:])
+
+#Create a string "developer" and print the characters from index 2 to 5.
+d="developer"
+print(d[2:5])
+
+#Take a string "hello world" and convert it into uppercase.
+y="hello world"
+print(y.upper())
+
+#Store your name in a variable and print it in uppercase letters.
+c="sanjana"
+print(c.upper())
+
+#Convert the string "PYTHON PROGRAMMING" into lowercase.
+l="PYTHON PROGRAMMING"
+print(l.lower())
+
+#Take the string "HeLLo PyTHoN" and convert it into lowercase.
+h="HeLLo PyTHoN"
+print(h.lower())
+
+#Remove the extra spaces from both sides of the string:" python "
+v=" python "
+print(v.strip())
+
+#Remove spaces from the left side of the string:" hello"
+x=" hello"
+print(x.lstrip())
+
+#Remove spaces from the right side of the string:"world "
+b="world "
+print(b.rstrip())
+
+
+
