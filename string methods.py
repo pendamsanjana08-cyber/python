9520a88dde6c89d5450980b6c2b18b7b1d570143
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-03-11 16:44:17 -0700

    string methods

---
+++ string methods.py
@@ -0,0 +1,39 @@
+#Question1: Convert the string "python" to uppercase.
+s = "python"
+print(s.upper())
+
+#Question2: Convert "HELLO" to lowercase.
+s = "HELLO"
+print(s.lower())
+
+#Question3: Capitalize the first letter of "python programming"
+s = "python programming"
+print(s.capitalize())
+
+#Question4: Replace "cat" with "dog" in "I like cat"
+s = "I like cat"
+print(s.replace("cat", "dog"))
+
+#Question5: Count how many "a" are in "banana".
+s = "banana"
+print(s.count("a"))
+
+#Question6: Find the position of "o" in "hello"
+s = "hello"
+print(s.find("o"))
+
+#Question7: Remove spaces from " hello ".
+s = "   hello   "
+print(s.strip())
+
+#Question8: Check if "python" starts with "py".
+s = "python"
+print(s.startswith("py"))
+
+#Question9: Check if "python" ends with "on".
+s = "python"
+print(s.endswith("on"))
+
+#Question10: Split "apple,banana,orange" using comma.
+s = "apple,banana,orange"
+print(s.split(","))
