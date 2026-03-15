44cd822be67311e4f5f1e20810c63d08e27197f4
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-03-15 10:59:46 -0700

    boolean

---
+++ boolean.py
@@ -0,0 +1,47 @@
+#Write a program that stores True in a variable and prints it.
+a=True
+print(a)
+
+#Write a program to check if 15 is greater than 10 and print the result.
+print(15>10)
+
+#Write a program to check if 20 is equal to 25 and print the Boolean result.
+print(20==25)
+
+#Create two variables a = 8 and b = 12.
+#Check if a is less than b.
+a=8
+b=12
+print(a<b)
+
+#Write a program to check if:
+#10 > 5 and 7 < 9
+#Print the result.
+print(10>5&7<9)
+
+#Write a program to check if:
+#4 > 10 or 8 < 12
+print(4 > 10 or 8 < 12)
+
+#Write a program that prints the opposite of True using the not operator.
+print(bool("none"))
+
+#Create two variables:
+#x = 50
+#y = 50
+#check if both values are equal.
+x = 50
+y = 50
+print(x==y)
+
+#Write a program that checks if 100 > 50.
+#If true, print "Yes, it is greater".
+
+
+'''Create two strings:
+a = "python"
+b = "Python"
+Check if both are equal and print the result'''
+a = "python"
+b = "Python"
+print(a==b)
