3f722ca2d3b0c3fae995b836710e394302a4ae56
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-03-17 11:17:19 -0700

    comparsionoperators

---
+++ comparsionoperators.py
@@ -0,0 +1,32 @@
+#check whether 15 is equal to 10
+a=10
+b=15
+c=10
+print(a==b)
+print(a==c)
+print(b==c)
+
+#use notequal symbol nd print result
+m=15
+o=4
+n=4
+print(o!=n)
+print(m!=n)
+
+#Write a program to check if 20 is greater than 15 and print the result.
+l=20
+k=15
+j=1>k
+print(j)
+
+#Create a variable:
+#marks = 75
+#Check if marks are greater than or equal to 50.
+marks=75
+if  (marks>=50):
+  print("marks are greater than or equal to 50")
+else:
+  print("lesser")
+  
+
+
