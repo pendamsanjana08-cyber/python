085d45dfcf017349a5256ce4fd75d10ea8a6f033
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-03-21 01:50:49 -0700

    sloved questions on assignment operators

---
+++ assignmentoperator.py
@@ -0,0 +1,97 @@
+#Create a variable x with value 10. Add 5 to it using an assignment operator and print the result.
+x=10
+x+=5
+print("after adding a is:",x)
+
+#Create a variable a with value 20. Subtract 7 from it using an assignment operator and print the result.
+a=20 
+a-=7
+print("after subtracting:",a)
+
+#Create a variable num with value 4. Multiply it by 3 using an assignment operator and print the result.
+num=4
+num*=3
+print(num)
+
+#Create a variable value with value 15. Divide it by 3 using an assignment operator and print the result.
+value = 15
+value/=3
+print(value)
+
+#Create a variable x with value 9. Perform floor division by 2 using an assignment operator and print the result.
+x=9
+x//=2
+print(x)
+
+#Create a variable x with value 5. First add 2 to it, then multiply the result by 3 using assignment operators. Print the final value.
+x=5
+x+=2
+x*=3
+print(x)
+
+#Create a variable a with value 50. Subtract 10 from it, then divide the result by 4 using assignment operators. Print the result.
+a=50
+a-=10
+a/=4
+print(a)
+
+#Create a variable num with value 10. First find the remainder when divided by 4, then add 6 using assignment operators. Print the result.
+num=10
+num%=4
+num+=6
+print(num)
+
+#Create a variable x with value 2. Raise it to the power of 3, then add 1 using assignment operators. Print the result.
+x=2
+x**=3
+x+=1
+print(x)
+
+#Create a variable y with value 100. Perform floor division by 3, then multiply the result by 2 using assignment operators. Print the final value.
+
+y=100
+y//=3
+y*=2
+print(y)
+
+#Create a variable x with value 10. Add the result of 5 multiplied by 2 to it using an assignment operator. Print the result.
+x=10
+x+=5*2
+print(x)
+
+#Create a variable a with value 8. Multiply it by the sum of 2 and 3 using an assignment operator. Print the result.
+a=8
+a*=2+3
+print(a)
+
+#Create a variable num with value 20. Divide it by 5, then add the result of 2 multiplied by 3 using assignment operators. Print the final result.
+num=20
+num/=5
+num+=2*3
+print(num)
+
+#Create a variable x with value 7. Find the remainder when divided by 3, then multiply the result by 4 using assignment operators. Print the result.
+x=7
+x%=3
+x*=4
+print(x)
+
+#Create a variable a with value 3. Raise it to the power of 2, then perform floor division by 2 using assignment operators. Print the result.
+a=3
+a**=2
+a//=2
+print(a)
+
+
+'''
+Create a variable x with value 5.
+Add 3, multiply by 2, subtract 4, and then perform floor division by 2 using assignment operators.
+Print the final result.
+'''
+
+x=5
+x+=3
+x*=2
+x-=4
+x//=2
+print(x)
--- comparsionoperators.py
+++ comparsionoperators.py
@@ -1,4 +1,5 @@
 #check whether 15 is equal to 10
+
 a=10
 b=15
 c=10
@@ -7,6 +8,7 @@ print(a==c)
 print(b==c)
 
 #use notequal symbol nd print result
+
 m=15
 o=4
 n=4
@@ -15,6 +17,7 @@ print(m!=n)
 
 #Write a program to check if 20 is greater than 15 and print the result.
 l=20
+
 k=15
 j=1>k
 print(j)
@@ -22,6 +25,7 @@ print(j)
 #Create a variable:
 #marks = 75
 #Check if marks are greater than or equal to 50.
+
 marks=75
 if  (marks>=50):
   print("marks are greater than or equal to 50")
