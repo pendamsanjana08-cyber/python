8ca3a210d9be3c1a5824f10e37c23e77fcf600b9
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-03-26 08:42:30 -0700

    comparsionoperators

---
+++ logicaloperator.py
@@ -0,0 +1,89 @@
+#Create two variables a = 10 and b = 5.
+#Check if both a > 5 and b > 2. Print the result.
+a=10
+b=5
+result = a>5 and b>2
+print("result  is:",result)
+
+#Create a variable num = 7.
+#Check if num is greater than 10 or equal to 7. Print the result.
+num=7
+result = num>10 or num==7
+print("result  is:",result)
+
+
+#Create a variable is_logged_in = False.
+#Use a logical operator to print the opposite value.
+is_logged_in = False
+result =  not(is_logged_in)
+print("result  is:",result)
+
+
+#Create two variables x = 3 and y = 8.
+#Check if x < 5 and y > 10. Print the result.
+x = 3 
+y = 8
+result = x < 5 and y > 10
+print("result  is:",result)
+
+
+#Create a variable marks = 40.
+#Check if marks is greater than 50 or equal to 40. Print the result.
+marks = 40
+result = marks>50 or marks==40
+print("result  is:",result)
+
+#Create a variable age = 20.
+#Check if age is between 18 and 25 using logical operators. Print the result.
+age = 20
+result=age>18 and age<25
+print(result)
+
+#Create a variable num = 15.
+#Check if num is divisible by both 3 and 5. Print the result.
+num = 15
+result = (num % 3 == 0) and (num % 5 == 0)
+print(result)
+
+#Create a variable temp = 30.
+#Check if temperature is less than 25 or greater than 35. Print the result.
+temp = 30
+print(temp<25 or temp>35 )
+
+#Create a variable x = 10.
+#Check if x is not greater than 15 using a logical operator. Print the result.
+x = 10
+print(x<15)
+
+#Create two variables a = 5 and b = 12.
+#Check if either a > 10 or b > 10. Print the result
+a = 5 
+b = 12
+print(a > 10 or b > 10)
+
+#Create a variable num = 22.
+#Check if the number is greater than 10 and not equal to 20. Print the result.
+num = 22
+print(num>10 and num!=20)
+
+#Create a variable age = 17.
+#Check if age is less than 18 or greater than 60. Print the result.
+age = 17
+print(age<18 or age>60)
+
+#Create a variable num = 9.
+#Check if the number is divisible by 3 and not divisible by 2. Print the result.
+num=9
+print((num % 3 == 0) and (num % 2 != 0))
+
+#Create a variable marks = 75.
+#Check if marks are between 50 and 100 and not equal to 60. Print the result.
+marks = 75
+print((marks > 50 and marks < 100) and (marks != 60))
+
+#Create two variables x = 8 and y = 4.
+#Check if (x > 5 and y < 10) or (x < 5). Print the result.
+x = 8  
+y = 4
+print((x > 5 and y < 10) or (x < 5))
+      
