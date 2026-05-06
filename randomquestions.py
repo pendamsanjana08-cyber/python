741bbb94445d5cd762757deec7a7bc1a42aa6ed2
pendamsanjana08-cyber <pendamsanjana08@gmail.com>
2026-05-06 22:31:11 +0530

    random question

---
+++ randomquestions.py
@@ -0,0 +1,33 @@
+''' Type Casting
+Write a program to:
+Convert "25" into integer
+Add 10
+Print the result'''
+
+a = "25"
+b = int(a)
+print(b + 10)
+
+
+
+''' List Operations
+Add "orange" to the list and print it.'''
+
+fruits = ["apple", "banana"]
+fruits.append("orange")
+print(fruits)
+
+
+'''Comparison Operator
+Check whether a is greater than b. '''
+
+a = 20
+b = 10
+print(a > b)
+
+
+''' Loop Concept
+Print numbers from 1 to 5 using a loop. '''
+
+for i in range(1,6):
+    print(i)
